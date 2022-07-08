8a781821eb0720f3f37298d4bc25e829db6fd08a
ISFlores <isaacfloresv@gmail.com>
2022-07-08 22:45:24 +0000

    Agregar node_modules and components js

---
+++ src/js/component/container_botton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const container_botton = () => {
+	return (
+		
+	);
+};
+
+export default Home;
---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div class="text-center">
+			<h1 class="text-center mt-5">copy right 2022</h1>			
+		</div>
+	);
+};
+
+export default Home;
